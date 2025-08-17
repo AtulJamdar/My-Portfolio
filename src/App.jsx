76214600e102fee3bbce78bce123893a4fd2ced3
AtulJamdar <work.atujamdar@gmail.com>
2025-08-17 13:44:03 +0530

    Initial commit: Setup React project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-green-400  flex items-center justify-center">App goes Here</h1>
+    </>
+  )
+}
+
+export default App
